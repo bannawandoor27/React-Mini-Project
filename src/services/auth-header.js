89ec17121051cf6f358cc4e92886bfe325b11788
hasanul banna <bannawandoor@gmail.com>
2023-02-26 12:11:05 +0530

    http req header added

---
+++ src/services/auth-header.js
@@ -0,0 +1,10 @@
+export default function auth_header() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    
+    if(user && user.accessToken) {
+       return { Authorization: `Bearer ${user.accessToken}`};
+    }else{
+        return {};
+    }
+       
+}
