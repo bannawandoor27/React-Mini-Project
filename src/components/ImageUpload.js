78482f0161b0df57e0a152923a56a13e4cb412e3
hasanul banna <bannawandoor@gmail.com>
2023-03-02 16:24:04 +0530

    unnecessary commits

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,11 +1,21 @@
 import React,{useState} from 'react'
 import UserService from '../services/user.service'
+
 function ImageUpload() {
+    const [image,setImage] = useState(null)
+   
+
     const handleFileChange = (e) => {
-        console.log(e.target.files[0])
+        setImage(e.target.files[0])
     }
     const handleFileUpload = (e) => {
-    }
+        const fd  = new FormData();
+        fd.append('image',image,image.name)
+        console.log(fd)
+        UserService.imageUpload(fd).then((res) => {
+            console.log(res)
+    })
+}
   return (
     <div>
         <input type='file' onChange={handleFileChange}   />
