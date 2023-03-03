6d57bd0634a9f8cbca6fe65d3fa818d13788fac7
hasanul banna <bannawandoor@gmail.com>
2023-03-03 08:00:33 +0530

    image header changed

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,18 +1,27 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import UserService from '../services/user.service'
 
 function ImageUpload() {
     const [image,setImage] = useState(null)
-   
+    useEffect(() => {
+        const formData = new FormData()
+        formData.append('filename',image);
+        UserService.imageUpload(formData).then((res)=>{
+
+        })
+      }, [image]);
 
     const handleFileChange = (e) => {
-        setImage(e.target.files[0])
+        setImage(e.target.files[0]);
+        console.log(e.target.files[0]);
+        console.log(image);
+        
     }
     const handleFileUpload = (e) => {
         const formData = new FormData()
         formData.append('filename',image);
         UserService.imageUpload(formData).then((res)=>{
-            console.log(res)
+
         })
     }
   return (
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,16 @@
 import axios from "axios";
 import auth_header from "./auth-header";
-
+const imageHeader = ()=> {
+        const user = JSON.parse(localStorage.getItem('user'));
+        if(user && user.token) {
+           return { 
+                Authorization: user.token,
+                'content-Type': 'multipart/form-data'
+         };
+        }else{
+            return {};
+        }        
+    }
 const API_URL = "http://localhost:8000/api/";
 
 const getPublicContent = () => axios.get(API_URL+'all');
@@ -9,7 +19,7 @@ const getUserBoard = () => axios.get(API_URL+'profile/',{headers: auth_header()}
 
 const getAdminBoard = () => axios.get(API_URL+'admin/',{headers: auth_header()+{}});
 
-const imageUpload = (file) => axios.patch(API_URL+'upload/', file, {headers: auth_header()+{'content-type':"multipart/form-data"}});
+const imageUpload = (file) => axios.put(API_URL+'upload/', file, {headers: imageHeader()});
 
 export default {
         getPublicContent,
